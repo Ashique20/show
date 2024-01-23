e53491f5b9a64f4cf23ba9d8f4b6297b8b804d33
Ashique <ashiqurrahman2092003@gmail.com>
2024-01-23 12:38:55 +0600

    button resolve

--- src/Components/Detail/Details.js
+++ src/Components/Detail/Details.js
@@ -62,7 +62,7 @@ const Detail =()=>{
   <input id="my-drawer" type="checkbox" className="drawer-toggle" />
   <div className="drawer-content">
     {/* Page content here */}
-    <label htmlFor="my-drawer" className="btn btn-primary drawer-button">Open drawer</label>
+    <label htmlFor="my-drawer" className="btn btn-primary drawer-button">Book Now</label>
   </div> 
   <div className="drawer-side">
     <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
